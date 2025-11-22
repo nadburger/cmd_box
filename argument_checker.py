e10332b85e7157c9b010db583bc2b0bec3442a09
nadburger <116915613+nadburger@users.noreply.github.com>
2025-11-22 21:19:11 +0200

    Fixed repo issues

--- argument_checker.py
+++ argument_checker.py
@@ -1,7 +1,7 @@
 import ctypes
 
 # Load DLL
-mylib = ctypes.CDLL(r"C:\Users\nadav\source\repos\cmd_box\x64\Debug\cmdbox.dll")
+mylib = input("The DLL path -> ")
 
 # fncmdbox(const wchar_t*)
 mylib.fncmdbox.argtypes = [ctypes.c_wchar_p]
